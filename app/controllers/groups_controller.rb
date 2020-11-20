4de49d6f7782bbcc127315dd68717d22340940c1
shi0280 <sho_ko504128@yahoo.co.jp>
2020-11-20 11:52:08 +0900

    edit-グループ登録

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,14 @@
 class GroupsController < ApplicationController  
-#   before_action :move_to_index, except: [:index]
-#   before_action :authenticate_user!, except: [:index]
+  before_action :move_to_index, except: [:index]
+  before_action :authenticate_user!, except: [:index]
 
-  # def index
-  #   @groups = Group.includes(:recruitments).order('created_at DESC')
-  # end
+  def index
+    @groups = Group.includes(:recruitments).order('created_at DESC')
+  end
 
   def new
-  #   follow = current_user.follow_okays.new
-  #   @users = User.where(id: follow.followed_id).or(User.where(id: follow.following_id))
+    follow = current_user.follow_okays.new
+    @users = User.where(id: follow.followed_id).or(User.where(id: follow.following_id))
     @group = Group.new
     @group.recruitments.new
     @group.users << current_user
@@ -24,26 +24,30 @@ class GroupsController < ApplicationController
     end
   end
 
-  # def update
-  #   @group = Group.find(params[:id])
-  #   if @group.update(recruitments_params)
-  #     redirect_to root_path, notice:"募集情報が変更されました。"
-  #   else
-  #     render :edit
-  #     flash.now[:alert] = "編集に失敗しました。"
-  #   end
-  # end
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(recruitments_params)
+      redirect_to root_path, notice:"募集情報が変更されました。"
+    else
+      render :edit
+      flash.now[:alert] = "編集に失敗しました。"
+    end
+  end
 
-  # def destroy
-  #   @group = Group.find(params[:id])
-  #   @group.destroy
-  #   redirect_to root_path, notice:"募集情報が削除されました。"
-  # end
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to root_path, notice:"募集情報が削除されました。"
+  end
 
   private
   
   def recruitment_params
     params.require(:group).permit(:name, user_ids: [], recruitments_attributes: [:title, :image, :adress, :description])
+  end
+  
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 
 end
--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -6,31 +6,4 @@ class RecruitmentsController < ApplicationController
     @groups = Group.includes(:recruitments).order('created_at DESC')
   end
 
-  def new
-    follow = current_user.follow_okays.new
-    @users = User.where(id: follow.followed_id).or(User.where(id: follow.following_id))
-    @group = Group.new
-    @group.recruitments.new
-    @group.users << current_user
-  end
-
-  def create
-  end
-
-  def edit
-    @group = Group.find(params[:id])
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
-  private
-
-  def move_to_index
-    redirect_to action: :index unless user_signed_in?
-  end
-
 end
