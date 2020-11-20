1abdcf5706fb03aa568059978612cfe8fa17c61e
shi0280 <sho_ko504128@yahoo.co.jp>
2020-11-21 00:39:43 +0900

    edit-募集検索

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,9 @@ class GroupsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @groups = Group.includes(:recruitments).order('created_at DESC')
+    @q = Group.includes(:recruitments).order('created_at DESC').ransack(params[:q])
+    @groups = @q.result(distinct: true)
+    @count = @q.result.count
   end
 
   def new
--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -3,7 +3,10 @@ class RecruitmentsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @groups = Group.includes(:recruitments).order('created_at DESC')
+    # @groups = Group.includes(:recruitments).order('created_at DESC')
+    @q = Group.includes(:recruitments).order('created_at DESC').ransack(params[:q])
+    @groups = @q.result(distinct: true)
+    @count = @q.result.count
   end
 
 end
